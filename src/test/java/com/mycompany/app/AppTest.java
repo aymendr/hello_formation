79baeccd19beead6897301108b78575a8a64ad8e
Aymen Drira <driraaymen@gmail.com>
2024-10-11 12:50:01 +0200

    update maths methods

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,4 +17,22 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
+    @Test
+    public void shoudAddNumbers(){
+        assertTrue(App.add(5, 2) == 8);
+    }
+
+    @Test
+    public void shoudMinsNumbers(){
+        assertTrue(App.mins(5, 2) == 3);
+    }
+
+    
+    @Test
+    public void shoudTimesNumbers(){
+        assertTrue(App.times(5, 2) == 3);
+    }
+
+    
 }
